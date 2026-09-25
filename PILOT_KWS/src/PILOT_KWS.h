ce89080cf5afb05983603489d721eb8966c58c54
Aarush <168774225+splash0e@users.noreply.github.com>
2026-09-26 00:39:53 +0530

    Add files via upload

---
+++ PILOT_KWS/src/PILOT_KWS.h
@@ -0,0 +1,30 @@
+#ifndef PILOT_KWS_H
+#define PILOT_KWS_H
+
+#include <Arduino.h>
+
+class PILOT_KWS_Class {
+public:
+
+    bool begin();
+
+    bool processAudio(const int16_t* audio, size_t samples);
+
+    bool isPilotDetected();
+
+    float getPilotConfidence();
+
+    float getBackgroundConfidence();
+
+    float getNegativeConfidence();
+
+private:
+
+    float pilotConfidence = 0.0f;
+    float backgroundConfidence = 0.0f;
+    float negativeConfidence = 0.0f;
+};
+
+extern PILOT_KWS_Class PILOT_KWS;
+
+#endif
